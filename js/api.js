dac24dc031391b514ea436d07c030095056e6aee
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-03 16:17:49 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,15 @@
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbzWXhYmtIo5ecM8Obg8PPRmnA262N1uUzTpQmRvYXuRI4jubsWvU2_cn3BiBPGuEmka/exec";
 
+async function savePrediction(data) {
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  return await response.json();
+}
