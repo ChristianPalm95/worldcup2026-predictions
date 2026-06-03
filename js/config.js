c7acaedd7990639b19e5cf26dde8699adf684b85
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-03 16:10:43 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,29 @@
+const CONFIG = {
+  competitionName:
+    "Business Finance - FIFA World Cup 2026 Prediction Challenge",
 
+  lockMinutesBeforeKickoff: 10,
+
+  bonusQuestions: {
+    worldChampion: 10,
+    goldenBoot: 5,
+    topScoringTeam: 3
+  },
+
+  allowedPlayers: [
+    "Store Mikkel",
+    "Lille Mikkel",
+    "Christian",
+    "Jacob",
+    "Jane",
+    "Daniel",
+    "Tobias",
+    "Zuzana",
+    "Georgiana",
+    "Maja",
+    "Iris",
+    "Lars"
+  ],
+
+  apiUrl: ""
+};
