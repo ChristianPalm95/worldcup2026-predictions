3ecfa251bf89cb1b9a2e14c8475f9dc736d97731
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-03 20:29:32 +0200

    Create matches.js

---
+++ js/matches.js
@@ -0,0 +1,51 @@
+const MATCHES = [
+
+  {
+    id: "A1",
+    group: "A",
+    kickoff: "2026-06-11T21:00:00+02:00",
+    home: "Mexico",
+    away: "South Africa"
+  },
+
+  {
+    id: "A2",
+    group: "A",
+    kickoff: "2026-06-12T04:00:00+02:00",
+    home: "South Korea",
+    away: "Czech Republic"
+  },
+
+  {
+    id: "A3",
+    group: "A",
+    kickoff: "2026-06-18T18:00:00+02:00",
+    home: "Czech Republic",
+    away: "South Africa"
+  },
+
+  {
+    id: "A4",
+    group: "A",
+    kickoff: "2026-06-19T03:00:00+02:00",
+    home: "Mexico",
+    away: "South Korea"
+  },
+
+  {
+    id: "A5",
+    group: "A",
+    kickoff: "2026-06-25T03:00:00+02:00",
+    home: "Czech Republic",
+    away: "Mexico"
+  },
+
+  {
+    id: "A6",
+    group: "A",
+    kickoff: "2026-06-25T03:00:00+02:00",
+    home: "South Africa",
+    away: "South Korea"
+  }
+
+];
