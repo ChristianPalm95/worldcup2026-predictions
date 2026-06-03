6a3107be4053cbc32280349a69339ef708c614bb
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-03 21:31:24 +0200

    Update matches.js

--- js/matches.js
+++ js/matches.js
@@ -46,6 +46,156 @@ const MATCHES = [
     kickoff: "2026-06-25T03:00:00+02:00",
     home: "South Africa",
     away: "South Korea"
-  }
+  },
+
+  // Group B
+
+{
+  id: "B1",
+  group: "B",
+  kickoff: "2026-06-12T21:00:00+02:00",
+  home: "Canada",
+  away: "Bosnia & Herzegovina"
+},
+
+{
+  id: "B2",
+  group: "B",
+  kickoff: "2026-06-13T21:00:00+02:00",
+  home: "Qatar",
+  away: "Switzerland"
+},
+
+{
+  id: "B3",
+  group: "B",
+  kickoff: "2026-06-18T21:00:00+02:00",
+  home: "Switzerland",
+  away: "Bosnia & Herzegovina"
+},
+
+{
+  id: "B4",
+  group: "B",
+  kickoff: "2026-06-19T00:00:00+02:00",
+  home: "Canada",
+  away: "Qatar"
+},
+
+{
+  id: "B5",
+  group: "B",
+  kickoff: "2026-06-24T21:00:00+02:00",
+  home: "Switzerland",
+  away: "Canada"
+},
+
+{
+  id: "B6",
+  group: "B",
+  kickoff: "2026-06-24T21:00:00+02:00",
+  home: "Bosnia & Herzegovina",
+  away: "Qatar"
+},
+
+// Group C
+
+{
+  id: "C1",
+  group: "C",
+  kickoff: "2026-06-14T00:00:00+02:00",
+  home: "Brazil",
+  away: "Morocco"
+},
+
+{
+  id: "C2",
+  group: "C",
+  kickoff: "2026-06-14T03:00:00+02:00",
+  home: "Haiti",
+  away: "Scotland"
+},
+
+{
+  id: "C3",
+  group: "C",
+  kickoff: "2026-06-20T00:00:00+02:00",
+  home: "Scotland",
+  away: "Morocco"
+},
+
+{
+  id: "C4",
+  group: "C",
+  kickoff: "2026-06-20T03:00:00+02:00",
+  home: "Brazil",
+  away: "Haiti"
+},
+
+{
+  id: "C5",
+  group: "C",
+  kickoff: "2026-06-25T00:00:00+02:00",
+  home: "Scotland",
+  away: "Brazil"
+},
+
+{
+  id: "C6",
+  group: "C",
+  kickoff: "2026-06-25T00:00:00+02:00",
+  home: "Morocco",
+  away: "Haiti"
+},
+
+// Group D
+
+{
+  id: "D1",
+  group: "D",
+  kickoff: "2026-06-13T03:00:00+02:00",
+  home: "USA",
+  away: "Paraguay"
+},
+
+{
+  id: "D2",
+  group: "D",
+  kickoff: "2026-06-14T06:00:00+02:00",
+  home: "Australia",
+  away: "Turkey"
+},
+
+{
+  id: "D3",
+  group: "D",
+  kickoff: "2026-06-19T21:00:00+02:00",
+  home: "USA",
+  away: "Australia"
+},
+
+{
+  id: "D4",
+  group: "D",
+  kickoff: "2026-06-20T06:00:00+02:00",
+  home: "Turkey",
+  away: "Paraguay"
+},
+
+{
+  id: "D5",
+  group: "D",
+  kickoff: "2026-06-26T04:00:00+02:00",
+  home: "Turkey",
+  away: "USA"
+},
+
+{
+  id: "D6",
+  group: "D",
+  kickoff: "2026-06-26T04:00:00+02:00",
+  home: "Paraguay",
+  away: "Australia"
+},
 
 ];
