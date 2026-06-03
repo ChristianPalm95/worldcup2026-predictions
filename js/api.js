d7b60d5904b39a98c55a1a58857bafba40ed180d
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-03 22:17:36 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbzWXhYmtIo5ecM8Obg8PPRmnA262N1uUzTpQmRvYXuRI4jubsWvU2_cn3BiBPGuEmka/exec";
+https://script.google.com/macros/s/AKfycbzWXhYmtIo5ecM8Obg8PPRmnA262N1uUzTpQmRvYXuRI4jubsWvU2_cn3BiBPGuEmka/exec";
 
 async function savePrediction(data) {
 
