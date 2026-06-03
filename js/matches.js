c9abd75337d842c9e46132f51d40425632ec07aa
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-03 21:43:45 +0200

    Update matches.js

--- js/matches.js
+++ js/matches.js
@@ -198,4 +198,204 @@ const MATCHES = [
   away: "Australia"
 },
 
+ // Group E
+
+{
+  id: "E1",
+  group: "E",
+  kickoff: "2026-06-14T19:00:00+02:00",
+  home: "Germany",
+  away: "Curacao"
+},
+
+{
+  id: "E2",
+  group: "E",
+  kickoff: "2026-06-15T01:00:00+02:00",
+  home: "Ivory Coast",
+  away: "Ecuador"
+},
+
+{
+  id: "E3",
+  group: "E",
+  kickoff: "2026-06-20T22:00:00+02:00",
+  home: "Germany",
+  away: "Ivory Coast"
+},
+
+{
+  id: "E4",
+  group: "E",
+  kickoff: "2026-06-21T02:00:00+02:00",
+  home: "Ecuador",
+  away: "Curacao"
+},
+
+{
+  id: "E5",
+  group: "E",
+  kickoff: "2026-06-25T22:00:00+02:00",
+  home: "Curacao",
+  away: "Ivory Coast"
+},
+
+{
+  id: "E6",
+  group: "E",
+  kickoff: "2026-06-25T22:00:00+02:00",
+  home: "Ecuador",
+  away: "Germany"
+},
+
+// Group F
+
+{
+  id: "F1",
+  group: "F",
+  kickoff: "2026-06-14T22:00:00+02:00",
+  home: "Netherlands",
+  away: "Japan"
+},
+
+{
+  id: "F2",
+  group: "F",
+  kickoff: "2026-06-15T04:00:00+02:00",
+  home: "Sweden",
+  away: "Tunisia"
+},
+
+{
+  id: "F3",
+  group: "F",
+  kickoff: "2026-06-20T19:00:00+02:00",
+  home: "Netherlands",
+  away: "Sweden"
+},
+
+{
+  id: "F4",
+  group: "F",
+  kickoff: "2026-06-21T06:00:00+02:00",
+  home: "Tunisia",
+  away: "Japan"
+},
+
+{
+  id: "F5",
+  group: "F",
+  kickoff: "2026-06-26T01:00:00+02:00",
+  home: "Japan",
+  away: "Sweden"
+},
+
+{
+  id: "F6",
+  group: "F",
+  kickoff: "2026-06-26T01:00:00+02:00",
+  home: "Tunisia",
+  away: "Netherlands"
+},
+
+// Group G
+
+{
+  id: "G1",
+  group: "G",
+  kickoff: "2026-06-15T21:00:00+02:00",
+  home: "Belgium",
+  away: "Egypt"
+},
+
+{
+  id: "G2",
+  group: "G",
+  kickoff: "2026-06-16T03:00:00+02:00",
+  home: "Iran",
+  away: "New Zealand"
+},
+
+{
+  id: "G3",
+  group: "G",
+  kickoff: "2026-06-21T21:00:00+02:00",
+  home: "Belgium",
+  away: "Iran"
+},
+
+{
+  id: "G4",
+  group: "G",
+  kickoff: "2026-06-22T03:00:00+02:00",
+  home: "New Zealand",
+  away: "Egypt"
+},
+
+{
+  id: "G5",
+  group: "G",
+  kickoff: "2026-06-27T05:00:00+02:00",
+  home: "Egypt",
+  away: "Iran"
+},
+
+{
+  id: "G6",
+  group: "G",
+  kickoff: "2026-06-27T05:00:00+02:00",
+  home: "New Zealand",
+  away: "Belgium"
+},
+
+// Group H
+
+{
+  id: "H1",
+  group: "H",
+  kickoff: "2026-06-15T18:00:00+02:00",
+  home: "Spain",
+  away: "Cape Verde"
+},
+
+{
+  id: "H2",
+  group: "H",
+  kickoff: "2026-06-16T00:00:00+02:00",
+  home: "Saudi Arabia",
+  away: "Uruguay"
+},
+
+{
+  id: "H3",
+  group: "H",
+  kickoff: "2026-06-21T18:00:00+02:00",
+  home: "Spain",
+  away: "Saudi Arabia"
+},
+
+{
+  id: "H4",
+  group: "H",
+  kickoff: "2026-06-22T00:00:00+02:00",
+  home: "Uruguay",
+  away: "Cape Verde"
+},
+
+{
+  id: "H5",
+  group: "H",
+  kickoff: "2026-06-27T02:00:00+02:00",
+  home: "Cape Verde",
+  away: "Saudi Arabia"
+},
+
+{
+  id: "H6",
+  group: "H",
+  kickoff: "2026-06-27T02:00:00+02:00",
+  home: "Uruguay",
+  away: "Spain"
+},
+
 ];
