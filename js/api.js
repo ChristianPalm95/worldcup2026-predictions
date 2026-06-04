bf0258fc6761f74546305aea96511b57c4d3897f
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-04 12:19:26 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,16 +1,24 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbzWXhYmtIo5ecM8Obg8PPRmnA262N1uUzTpQmRvYXuRI4jubsWvU2_cn3BiBPGuEmka/exec";
+  "https://script.google.com/macros/s/AKfycbzWXhYmtIo5ecM8Obg8PPRmnA262N1uUzTpQmRvYXuRI4jubsWvU2_cn3BiBPGuEmka/exec";
 
 async function savePrediction(data) {
 
-  await fetch(API_URL, {
+  const response = await fetch(API_URL, {
     method: "POST",
-    mode: "no-cors",
     body: JSON.stringify(data)
   });
 
-  return {
-    success: true
-  };
+  return await response.json();
+
+}
+
+async function loadPrediction(player) {
+
+  const response =
+    await fetch(
+      `${API_URL}?player=${encodeURIComponent(player)}`
+    );
+
+  return await response.json();
 
 }
