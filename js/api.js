8321c6a5d3e55495b0ec51ce49bad8d693520a91
ChristianPalm95 <christian.yde.palm@hotmail.com>
2026-06-04 09:10:19 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,13 +1,6 @@
-const API_URL =
-"https://script.google.com/macros/s/AKfycbzWXhYmtIo5ecM8Obg8PPRmnA262N1uUzTpQmRvYXuRI4jubsWvU2_cn3BiBPGuEmka/exec";
-
 async function savePrediction(data) {
 
-  await fetch(API_URL, {
-    method: "POST",
-    mode: "no-cors",
-    body: JSON.stringify(data)
-  });
+  console.log("API CALLED");
 
   return {
     success: true
